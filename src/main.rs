0955867807aecb3724fa1de9e5cbe1eaa72df609
Neil Pate <neil@premiergsolutions.com>
2022-12-12 21:58:53 +0100

    feat: Added minifb

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use minifb::{Key, Scale, Window, WindowOptions};
+use std::time::{Duration, Instant};
+
 mod threed;
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
 
 fn main() {
     println!("Hello, world!");
 
     let _obj = threed::Object::create_from_file("c:\\temp\\cube.obj".to_string());
-
-    // match obj {
-    //     Ok(obj) => println!("ok"),//print!("Object loaded: {obj:#?}"),
-    //     Err(_) => (),
-    // }
 
     let screen = threed::Screen {
         width: 800,
@@ -45,18 +46,81 @@ fn main() {
     //Do backface culling
     //For each point in the triangle do the mult_vec_matrix with the view matrix to get a new triangle
 
-    // for tri in obj.unwrap().tris  {
-    //     //let new_v1 = tri.v1;
-    //     let v1_trans = threed::mult_vec_matrix(array![tri.v1.x, tri.v1.y, tri.v1.z], &tm);
-    //     let v2_trans = threed::mult_vec_matrix(array![tri.v2.x, tri.v2.y, tri.v2.z], &tm);
-    //     let v3_trans = threed::mult_vec_matrix(array![tri.v3.x, tri.v3.y, tri.v3.z], &tm);
+    let mut window = Window::new(
+        "Plasma + Text Example",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            resize: false,
+            scale: Scale::X1,
+            ..WindowOptions::default()
+        },
+    )
+    .expect("Unable to create window");
 
-    //     println!("{}", v1_trans);
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_millis(10)));
 
-    //     let v1_view = threed::mult_vec_matrix(v1_trans, &vm);
-    //     let v2_view = threed::mult_vec_matrix(v2_trans, &vm);
-    //     let v3_view = threed::mult_vec_matrix(v3_trans, &vm);
-    // }
+    let mut buffer: Vec<u32> = Vec::with_capacity(WIDTH * HEIGHT);
 
-    // let tris = obj.as_ref().unwrap().tris;
+    let mut size = (0, 0);
+    let mut time = 0.0f32;
+
+    //  let status_text = StatusText::new(WIDTH, HEIGHT, 2);
+
+    let mut prev = Instant::now();
+    let mut count = 0;
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        let new_size = (window.get_size().0, window.get_size().1);
+        if new_size != size {
+            size = new_size;
+            buffer.resize(size.0 * size.1, 0);
+        }
+
+        let y_step = 1.0 / HEIGHT as f32;
+        let x_step = 1.0 / WIDTH as f32;
+        let mut y = 0.0;
+
+        for yi in 0..HEIGHT {
+            let buf = &mut buffer[yi * WIDTH..(yi + 1) * WIDTH];
+            let mut x = 0.0f32;
+
+            // So this code is really slow, but good enough as example :)
+            for xi in 0..WIDTH {
+                let k = 0.1 + (y + (0.148 - time).sin()).cos() + 2.4 * time;
+                let w = 0.9 + (x + (0.628 + time).cos()).cos() - 0.7 * time;
+                let d = (x * x + y * y).sqrt();
+                let s = 7.0 * (d + w).cos() * (k + w).sin();
+                let r = ((s + 0.2).cos() * 255.0) as u32;
+                let g = ((s + 0.5).cos() * 255.0) as u32;
+                let b = ((s + 0.7).cos() * 255.0) as u32;
+                buf[xi] = (r << 16) | (g << 8) | b;
+
+                x += x_step;
+            }
+
+            y += y_step;
+        }
+
+        let mut current = Instant::now();
+        count += 1;
+
+        if count == 1000 {
+            count = 0;
+            let delta_time = (current - prev).as_secs_f32();
+            let frame_rate = 1. / delta_time;
+
+            //   println!("{frame_rate:.1} FPS",);
+            println!("{frame_rate:.1} FPS",);
+        }
+
+        prev = current;
+
+        window
+            .update_with_buffer(&buffer, new_size.0, new_size.1)
+            .unwrap();
+
+        time += 1.0 / 60.0;
+    }
 }
